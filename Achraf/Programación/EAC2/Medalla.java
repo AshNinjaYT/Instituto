33e653c216fb5355a8baf04d9f1221f4332aaf9c
AshNinjaYT <achraf.b.t.s2006@gmail.com>
2024-10-23 14:23:24 +0200

    23/10/2024 2

---
+++ Achraf/Programación/EAC2/Medalla.java
@@ -0,0 +1,30 @@
+package EAC2.Enunciat;
+
+public class Medalla {
+
+    static final int MEDALLA_OR = 1;
+    static final int MEDALLA_ARGENT = 2;
+    static final int MEDALLA_BRONZE = 3;
+
+    private static int numMedallesRepartides = 0;
+  
+    int posicio;
+    Atleta atleta;
+
+    public static int getNumMedallesRepartides() {
+        return numMedallesRepartides;
+    }
+
+    Medalla(int posicio, Atleta guanyador) {
+        this.posicio = posicio;
+        this.atleta = guanyador;
+        atleta.pais.novaMedalla(this);
+        atleta.guanyarMedalla(this);
+        numMedallesRepartides++;
+    }
+
+    public String descripcio() {
+        return  "L'atleta " + atleta.nom + " ha guanyat una medalla en obtenir la posició " + posicio + " a la seva prova de " + atleta.disciplina;
+    }
+
+}
---
+++ Achraf/Programación/Ej3/Aprova.java
@@ -0,0 +1,5 @@
+public class Aprova {
+    public static void main(String[] args) {
+        
+    }
+}
