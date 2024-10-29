1f6634e8f3848e5c9a959e042e463a74b7c7663c
AshNinjaYT <achraf.b.t.s2006@gmail.com>
2024-10-29 23:04:25 +0100

    29/10/2024

--- Achraf/Programación/Ej3/Aprova.java
+++ Achraf/Programación/Ej3/Aprova.java
@@ -1,5 +1,10 @@
 public class Aprova {
     public static void main(String[] args) {
-        
+        int nota = 50;
+        if (nota >= 50) {
+            System.out.println("Ha aprovado");
+        } else {
+            System.our.println("Ha suspendido");
+        }
     }
 }
