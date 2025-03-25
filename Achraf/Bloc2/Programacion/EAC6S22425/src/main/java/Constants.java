24c835eb7928dcdf1215d95143e75cf0e0a91ee7
AshNinjaYT <achraf.b.t.s2006@gmail.com>
2025-03-25 15:33:43 +0100

    cambios

--- Achraf/Bloc2/Programacion/EAC6S22425/src/main/java/Constants.java
+++ Achraf/Bloc2/Programacion/EAC6S22425/src/main/java/Constants.java
@@ -1,8 +1,7 @@
 public class Constants {
 
-    public static final String FILE_NAME = ""; //Vacio hasta que añadan un archivo
+    public static final String FILE_NAME = ""; // Vacio hasta que añadan un archivo
     public static final String DEFAULT_DATA_DIRECTORY = "data";
-
 
     public static final String MESSAGE_DEFAULT_ASK_STRING = "Introdueixi una cadena de text";
     public static final String MESSAGE_DEFAULT_ERROR_STRING = "S'ha introduït un text buit";
@@ -13,8 +12,10 @@ public class Constants {
     public static final String MESSAGE_DEFAULT_ERROR_FLOAT = "El valor introduït no correspon a un nombre amb decimals";
     public static final String MESSAGE_ASK_OPTION_VALUE = "Introdueixi un valor enter per l'opció";
     public static final String MESSAGE_NOT_VALID_OPTION = "No s'ha introduït una opció vàlida";
-    
-    
+    public static final String ERROR_NO_INTEGER = "No s'ha introduït un número enter";
+    public static final String ERROR_NO_FLOAT = "No s'ha introduït un número amb decimals";
+    public static final String ERROR_EMPTY_STRING = "S'ha introduït un text buit";
+
     public static final String MESSAGE_INSERT_MARKET_NAME = "Introdueixi el nom del supermercat";
     public static final String MESSAGE_INSERT_CITY = "A quina ciutat es troba?";
     public static final String MESSAGE_INSERT_LONGITUDE = "Introdueixi la longitud on es troba el supermercat";
@@ -23,9 +24,7 @@ public class Constants {
     public static final String MESSAGE_CONFIRMED_DELETION = "Arxiu esborrat i creat de nou satisfactòriament";
     public static final String MESSAGE_FILES_CREATED = "Supermercats i productes exportats correctament";
     public static final String MESSAGE_FILES_IMPORTED = "Supermercats i productes importats correctament";
-    public static final String ERROR_NO_INTEGER = "No s'ha introduït un número enter";
-    public static final String ERROR_NO_FLOAT = "No s'ha introduït un número amb decimals";
-    public static final String ERROR_EMPTY_STRING = "S'ha introduït un text buit";
+
     public static final String ERROR_DELETION_CANCELED = "Cancel·lat per petició de l'usuari";
     public static final String ERROR_DELETION = "No s'ha pogut esborrar l'arxiu";
     public static final String ERROR_FILE_NAME_NULL = "El nom de l'arxiu no pot ser null o buit";
@@ -36,22 +35,21 @@ public class Constants {
     public static final String ERROR_CONTENT_NULL = "El contingut de l'arxiu no pot ser null o buit";
     public static final String ERROR_FILES_NOT_FOUND = "Arxius no trobats";
 
-
     public static final String MESSAGE_SEPARATOR = "--------------------------------------------------------------------------------------------------------------";
     public static final String START_MENU_HEADER = "GESTIO IOC SUPERMARKETS";
     public static final String START_MENU = """
-                                    1) Afegir supermercat.
-                                    2) Afegir producte a un supermercat.
-                                    3) Veure supermercats.
-                                    4) Exportar supermercats i preus.
-                                    5) Importar supermercats i preus.
-                                    0) Sortir.
-                                    """;    
+            1) Afegir supermercat.
+            2) Afegir producte a un supermercat.
+            3) Veure supermercats.
+            4) Exportar supermercats i preus.
+            5) Importar supermercats i preus.
+            0) Sortir.
+            """;
     public final static String LIST_HEADER = "SUPERMARKET         CITY                      LONGITUDE    LATITUDE";
     public final static String TEMP_LIN = "-----------------------------------------------------------------------------";
     public static final String EMPTY_SPACE = "";
-    public static final String ERROR_HEADER = "ERROR";   
-    public static final String INFO_HEADER = "INFO";   
+    public static final String ERROR_HEADER = "ERROR";
+    public static final String INFO_HEADER = "INFO";
     public static final String DATE_FORMAT = "yyyyMMddHHmm";
     public final static String SUPERMARKET_BOARD_FORMAT = "%-18s  %-24s %11.7f %11.7f";
     public final static String SUPERMARKET_SPLIT_FORMAT = "%s,%s,%.7f,%.7f\n";
--- Achraf/Bloc2/Programacion/EAC6S22425/src/main/java/EAC6S22425.java
+++ Achraf/Bloc2/Programacion/EAC6S22425/src/main/java/EAC6S22425.java
@@ -1,11 +1,59 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class EAC6S22425 {
+    private SupermarketList supermarketList;
+    private Scanner scanner;
 
     public static void main(String[] args) {
         EAC6S22425 program = new EAC6S22425();
         program.start();
     }
 
-    public void start() {
-       
+    public EAC6S22425() {
+        supermarketList = new SupermarketList();
+        scanner = new Scanner(System.in);
     }
+
+    public void start() {
+        Supermarket su;
+        UtilsIO io = new UtilsIO();
+        FileUtils fu = new FileUtils(Constants.DEFAULT_DATA_DIRECTORY);
+        while (true) {
+            io.showMenu(Constants.START_MENU);
+            int option = io.askForInteger(Constants.MESSAGE_ASK_OPTION_VALUE, Constants.ERROR_NO_INTEGER);
+            switch (option) {
+                case 1:
+                    enterSupermarketData(io, fu,su);
+                    break;
+                case 2:
+                    addProduct();
+                    break;
+                case 3:
+                    listSupermarkets();
+                    break;
+                case 4:
+                    exportSupermarkets();
+                    break;
+                case 5:
+                    importSupermarkets();
+                    break;
+                case 0:
+                    return;
+                default:
+                    io.showError(Constants.MESSAGE_NOT_VALID_OPTION);
+                    ;
+            }
+        }
+    }
+
+    private void enterSupermarketData(UtilsIO io, FileUtils fu, Supermarket su) {
+        String supermarketName = io.askForString(Constants.MESSAGE_INSERT_MARKET_NAME, Constants.MESSAGE_ERROR_EMPTY_STRING);
+        String supermarketCity = io.askForString(Constants.MESSAGE_INSERT_CITY, Constants.MESSAGE_ERROR_EMPTY_STRING);
+        float supermarketLongitude = io.askForFloat(Constants.MESSAGE_INSERT_LONGITUDE, Constants.MESSAGE_ERROR_NO_FLOAT);
+        float supermarketLatitude = io.askForFloat(Constants.MESSAGE_INSERT_LATITUDE, Constants.MESSAGE_ERROR_NO_FLOAT);
+        su.
+        
+        fu.insertMarketInfoIntoFile(supermarketName, supermarketCity, supermarketLongitude, supermarketLatitude);
+    }          
 }
